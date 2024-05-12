747197e139ad40654c202672dc12851e98bdfde3
Volteee <maksim-kim06@mail.ru>
2024-05-12 20:04:42 +0300

    Test

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const http = require('node:http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-
-const port = 8081;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+app.get('/', (req, res) => {
+  res.send('Привет, мир!');
 });
 
-server.listen(port, () => {
-  console.log(`Server running at http://:${port}/`);
-}); 
+app.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
+});
