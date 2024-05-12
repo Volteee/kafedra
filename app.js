4355a569ecf887ee6e6066a66bda6506aa47d091
Volteee <maksim-kim06@mail.ru>
2024-05-12 19:58:36 +0300

    Test

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running at http://${}:${port}/`);
+  console.log(`Server running at http://:${port}/`);
 }); 
