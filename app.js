3efabbb258a8ed5052215e5eca110528c99196ea
Volteee <maksim-kim06@mail.ru>
2024-05-12 20:28:21 +0300

    Test

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var http = require('http'),
-    fs = require('fs');
-
-
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+ 
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
+ 
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 8082);
+ 
+console.log('Running at Port 8082');
