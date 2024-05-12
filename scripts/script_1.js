bf88c5917514b3e962a3cc89267a358034907b03
Volteee <maksim-kim06@mail.ru>
2024-05-12 19:39:18 +0300

    first commit

---
+++ scripts/script_1.js
@@ -0,0 +1,16 @@
+var typed = new Typed('.typed', {
+  strings: [
+    '<span class="grad0">Web Developer</span>',
+    '<span class="grad1">Data Scientist</span>',
+    '<span class="grad2">Product Manager</span>',
+    '<span class="grad3">Systems Analyst</span>',
+    '<span class="grad4">Big Data Engineer</span>',
+    '<span class="grad5">Software Engineer</span>',
+    '<span class="grad6">Fullstack Engineer</span>',
+  ],
+  typeSpeed: 50,
+  backSpeed: 30,
+  backDelay: 3000,
+  loop: true,
+})
+
